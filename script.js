e79a04818c85927b204e9e1d997e7caf9a6f9443
pedrozimermann <phzpaiva@gmail.com>
2024-09-28 16:40:51 -0300

    Substituição de formato de imagem

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 function toggleMode() {
   const html = document.documentElement
-  html.classList.toggle("light");
+  html.classList.toggle("light")
 
-const img = document.querySelector("img");
+  const img = document.querySelector("img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png");
-    img.setAttribute("alt", "Avatar Light");
+    img.setAttribute("src", "./assets/avatar-light.jpg")
+    img.setAttribute("alt", "Avatar Light")
   } else {
-    img.setAttribute("src", "./assets/avatar.png");
-    img.setAttribute("alt", "Avatar");
+    img.setAttribute("src", "./assets/avatar.jpg")
+    img.setAttribute("alt", "Avatar")
   }
 
-
-
   //pegar a tag img
-
 
   //substituir o src da imagem
   //se tiver light mode, adicionar a imagem light
