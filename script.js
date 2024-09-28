974188ce746d100fd20041c6806bf605902d8d27
pedrozimermann <phzpaiva@gmail.com>
2024-09-28 09:40:00 -0300

    Update

---
+++ script.js
@@ -0,0 +1,23 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light");
+
+const img = document.querySelector("img");
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png");
+    img.setAttribute("alt", "Avatar Light");
+  } else {
+    img.setAttribute("src", "./assets/avatar.png");
+    img.setAttribute("alt", "Avatar");
+  }
+
+
+
+  //pegar a tag img
+
+
+  //substituir o src da imagem
+  //se tiver light mode, adicionar a imagem light
+  //se não tiver light mode, adicionar a imagem normal
+}
